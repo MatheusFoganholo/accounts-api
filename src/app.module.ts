09bd33e9a29f44880e10c20621b10a6fdc4fa783
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-08-28 15:21:40 -0300

    feat: change cluster information

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AccountsModule } from './modules/accounts/account.module';
+
+import { AccountsModule } from '~/modules/accounts/account.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://matheus:crudAccountsPassword@crud-accounts.b0jxb.mongodb.net/test',
+      'mongodb+srv://matheus:accountsPassword@accounts.bgt2p.mongodb.net/accounts?retryWrites=true&w=majority',
     ),
     AccountsModule,
   ],
